e5c161f1fb80d2f37ebc0094c3401d3c27817d70
Roman Timashev <tmshv@yandex-team.ru>
2017-10-03 21:49:24 +0300

    Project 4 spaces -> tabs

--- src/app/features/Project/Project.js
+++ src/app/features/Project/Project.js
@@ -7,95 +7,95 @@ import {connect} from 'react-redux'
 
 import './Project.css'
 
-const Loader = ({text='Loading'}) => <i>{text}</i>
+const Loader = ({text = 'Loading'}) => <i>{text}</i>
 
 function mapStateToProps(state) {
-    return {
-        project: state.app.projects.activeProject
-    }
+	return {
+		project: state.app.projects.activeProject
+	}
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actions, dispatch)
+	return bindActionCreators(actions, dispatch)
 }
 
 const HTML = ({children}) => (
-    <div dangerouslySetInnerHTML={{__html: children}}/>
+	<div dangerouslySetInnerHTML={{__html: children}}/>
 )
 
 class Project extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            content: null,
-            contentUrl: null,
-        }
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			content: null,
+			contentUrl: null,
+		}
+	}
 
-    componentDidMount() {
-        const {project} = this.props
-        if (project) this.initProject(project)
-    }
+	componentDidMount() {
+		const {project} = this.props
+		if (project) this.initProject(project)
+	}
 
-    componentWillReceiveProps(nextProps) {
-        const {project} = nextProps
-        if (project) this.initProject(project)
-    }
+	componentWillReceiveProps(nextProps) {
+		const {project} = nextProps
+		if (project) this.initProject(project)
+	}
 
-    initProject(project) {
-        if (project.content) {
-            this.setState({
-                content: project.content,
-            })
-        } else if (project.contentUrl) {
-            this.setState({
-                contentUrl: project.contentUrl,
-            })
-            // return
-            fetch(project.contentUrl)
-                .then(res => res.text())
-                .then(content => {
-                    this.setState({
-                        content,
-                    })
-                })
-        }
-    }
+	initProject(project) {
+		if (project.content) {
+			this.setState({
+				content: project.content,
+			})
+		} else if (project.contentUrl) {
+			this.setState({
+				contentUrl: project.contentUrl,
+			})
+			// return
+			fetch(project.contentUrl)
+				.then(res => res.text())
+				.then(content => {
+					this.setState({
+						content,
+					})
+				})
+		}
+	}
 
-    render() {
-        const {content} = this.state
+	render() {
+		const {content} = this.state
 
-        if (!content) {
-            return (
-                <div className="Project">
-                    <Loader/>
-                </div>
-            )
-        }
+		if (!content) {
+			return (
+				<div className="Project">
+					<Loader/>
+				</div>
+			)
+		}
 
-        const {project} = this.props
-        return (
-            <div className="Project">
-                <header>
-                    <h1>{project.title}</h1>
-                    <hr/>
-                </header>
+		const {project} = this.props
+		return (
+			<div className="Project">
+				<header>
+					<h1>{project.title}</h1>
+					<hr/>
+				</header>
 
-                <div className="ProjectBody">
-                    <HTML>
-                        {content}
-                    </HTML>
-                </div>
+				<div className="ProjectBody">
+					<HTML>
+					{content}
+					</HTML>
+				</div>
 
-                {/*<div>*/}
-                {/*<ul>*/}
-                {/*<li><Link to="/projects">All Projects</Link></li>*/}
-                {/*<li><Link to="/">Close</Link></li>*/}
-                {/*</ul>*/}
-                {/*</div>*/}
-            </div>
-        )
-    }
+				{/*<div>*/}
+				{/*<ul>*/}
+				{/*<li><Link to="/projects">All Projects</Link></li>*/}
+				{/*<li><Link to="/">Close</Link></li>*/}
+				{/*</ul>*/}
+				{/*</div>*/}
+			</div>
+		)
+	}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Project)
